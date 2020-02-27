56652932b6d7ef7c806c83f52a896296d6ff5d58
Paul Hebert <paul@cloudfour.com>
2020-02-27 13:51:55 -0800

    Scaffold out new test

---
+++ cypress/integration/delete_item.spec.js
@@ -0,0 +1,9 @@
+describe('TODO: Workflow', function() {
+  // Test adding multiple items
+
+  // Test marking an item as complete
+
+  // Test deleting an item
+
+  // Test deleting the final item
+});
