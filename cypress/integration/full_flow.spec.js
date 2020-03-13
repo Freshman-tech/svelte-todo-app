3f42dbba4d8b1d1dde40c6a8d10d90198b568ccc
Paul Hebert <paul@cloudfour.com>
2020-03-13 16:22:15 -0700

    Initial refactor of tests
    - Removed describe block since it only housed one test
    - Commented out logs — left them in for demo purposes
    - Refactored `get` statements to follow best practices.
        - Don't fetch by class
        - Fetch by data attribute or text instead
    
    Co-authored-by: Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>

--- cypress/integration/full_flow.spec.js
+++ cypress/integration/full_flow.spec.js
@@ -1,80 +1,78 @@
-describe("TODO: Workflow", function() {
-  /**
-   * Refactoring considerations
-   * @see https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
-   * Do we need logs?
-   * Can we DRY the get() at all?
-   */
-  it("Bla", function() {
-    cy.visit("/");
+/**
+ * Refactoring considerations
+ * @see https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
+ * Can we DRY the get() at all?
+ */
+it("Should add, complete, and remove items", function() {
+  cy.visit("/");
 
-    const PLACEHOLDER_VAL = "[placeholder='E.g. Build a web app']";
+  const PLACEHOLDER_VAL = "[placeholder='E.g. Build a web app']";
+  const EMPTY_STATE_TEXT = "Add your first todo";
 
-    cy.log("Check our empty state...");
-    cy.contains("Add your first todo").should("be.visible");
+  // cy.log("Check our empty state...");
+  cy.contains(EMPTY_STATE_TEXT).should("be.visible");
 
-    cy.log('Enter "test 1" into form input');
-    cy.get(PLACEHOLDER_VAL).type("test 1");
+  // cy.log('Enter "test 1" into form input');
+  cy.get(PLACEHOLDER_VAL).type("test 1");
 
-    cy.log('Press "enter" to submit our form');
-    cy.get(PLACEHOLDER_VAL).type("{enter}");
+  // cy.log('Press "enter" to submit our form');
+  cy.get(PLACEHOLDER_VAL).type("{enter}");
 
-    cy.log("Check that there is a ToDo list item with the text 'test 1'");
-    cy.get(".todo-item").should("contain", "test 1");
+  // cy.log("Check that there is a ToDo list item with the text 'test 1'");
+  cy.get("[data-cy='todo-item']").should("contain", "test 1");
 
-    cy.log("Check our empty state has disappeared");
-    cy.contains("Add your first todo").should("not.be.visible");
+  // cy.log("Check our empty state has disappeared");
+  cy.contains(EMPTY_STATE_TEXT).should("not.be.visible");
 
-    cy.log("Add 2 more items");
-    cy.get(".js-todo-input")
-      .type("test 2")
-      .type("{enter}");
-    cy.get(".js-todo-input")
-      .type("test 3")
-      .type("{enter}");
+  // cy.log("Add 2 more items");
+  cy.get(PLACEHOLDER_VAL)
+    .type("test 2")
+    .type("{enter}");
+  cy.get(PLACEHOLDER_VAL)
+    .type("test 3")
+    .type("{enter}");
 
-    cy.log("Check that there are now 3 items");
-    cy.get(".todo-item").should("have.length", 3);
+  // cy.log("Check that there are now 3 items");
+  cy.get("[data-cy='todo-item']").should("have.length", 3);
 
-    cy.log("Mark the first item as done");
-    cy.get(".tick")
-      .first()
-      .click();
+  // cy.log("Mark the first item as done");
+  cy.get("[data-cy='visual-checkbox']")
+    .first()
+    .click();
 
-    cy.log("Confirm the first item has the done class");
-    cy.get(".todo-item")
-      .first()
-      .should("have.class", "done");
+  // cy.log("Confirm the first item has the done class");
+  cy.get("[data-cy='todo-item']")
+    .first()
+    .should("have.class", "done");
 
-    cy.log("Confirm the first item's checkbox is checked'");
-    cy.get(".todo-item [type='checkbox']")
-      .first()
-      .should("be.checked");
+  // cy.log("Confirm the first item's checkbox is checked'");
+  cy.get("[data-cy='hidden-checkbox']")
+    .first()
+    .should("be.checked");
 
-    cy.log("Delete our first item by clicking the 'X' button");
-    cy.contains("li", "test 2")
-      .find(".delete-todo")
-      .click();
+  // cy.log("Delete our first item by clicking the 'X' button");
+  cy.contains("[data-cy='todo-item']", "test 2")
+    .find("[data-cy='delete-button']")
+    .click();
 
-    cy.log("Check that there are now 2 items");
-    cy.get(".todo-item").should("have.length", 2);
+  // cy.log("Check that there are now 2 items");
+  cy.get("[data-cy='todo-item']").should("have.length", 2);
 
-    cy.log("Delete our remaining elements");
-    cy.contains("li", "test 1")
-      .find(".delete-todo")
-      .click();
+  // cy.log("Delete our remaining elements");
+  cy.contains("[data-cy='todo-item']", "test 1")
+    .find("[data-cy='delete-button']")
+    .click();
 
-    cy.log("Check that there is now 1 item");
-    cy.get(".todo-item").should("have.length", 1);
+  // cy.log("Check that there is now 1 item");
+  cy.get("[data-cy='todo-item']").should("have.length", 1);
 
-    cy.contains("li", "test 3")
-      .find(".delete-todo")
-      .click();
+  cy.contains("[data-cy='todo-item']", "test 3")
+    .find("[data-cy='delete-button']")
+    .click();
 
-    cy.log("Check that there are now 0 items");
-    cy.get(".todo-item").should("not.exist");
+  // cy.log("Check that there are now 0 items");
+  cy.get("[data-cy='todo-item']").should("not.exist");
 
-    cy.log("Check our empty state...");
-    cy.contains("Add your first todo").should("be.visible");
-  });
+  // cy.log("Check our empty state...");
+  cy.contains(EMPTY_STATE_TEXT).should("be.visible");
 });
--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
-  import { afterUpdate } from 'svelte';
+  import { afterUpdate } from "svelte";
 
   afterUpdate(() => {
-    document.querySelector('.js-todo-input').focus();
+    document.querySelector(".js-todo-input").focus();
   });
 
   let todoItems = [];
-  let newTodo = '';
+  let newTodo = "";
 
   function addTodo() {
     newTodo = newTodo.trim();
@@ -15,11 +15,11 @@
     const todo = {
       text: newTodo,
       checked: false,
-      id: Date.now(),
+      id: Date.now()
     };
 
     todoItems = [...todoItems, todo];
-    newTodo = '';
+    newTodo = "";
   }
 
   function toggleDone(id) {
@@ -37,23 +37,41 @@
     <h1 class="app-title">todos</h1>
     <ul class="todo-list">
       {#each todoItems as todo (todo.id)}
-        <li class="todo-item {todo.checked ? 'done' : ''}">
-          <input id={todo.id} type="checkbox" />
-          <label for={todo.id} class="tick" on:click={() => toggleDone(todo.id)}></label>
+        <li class="todo-item {todo.checked ? 'done' : ''}" data-cy="todo-item">
+          <input id={todo.id} type="checkbox" data-cy="hidden-checkbox" />
+          <label
+            for={todo.id}
+            class="tick"
+            data-cy="visual-checkbox"
+            on:click={() => toggleDone(todo.id)} />
           <span>{todo.text}</span>
-          <button class="delete-todo" on:click={() => deleteTodo(todo.id)}>
-            <svg><use href="#delete-icon"></use></svg>
+          <button
+            class="delete-todo"
+            on:click={() => deleteTodo(todo.id)}
+            data-cy="delete-button">
+            <svg>
+              <use href="#delete-icon" />
+            </svg>
           </button>
         </li>
       {/each}
     </ul>
     <div class="empty-state">
-      <svg class="checklist-icon"><use href="#checklist-icon"></use></svg>
+      <svg class="checklist-icon">
+        <use href="#checklist-icon" />
+      </svg>
       <h2 class="empty-state__title">Add your first todo</h2>
-      <p class="empty-state__description">What do you want to get done today?</p>
+      <p class="empty-state__description">
+        What do you want to get done today?
+      </p>
     </div>
     <form on:submit|preventDefault={addTodo}>
-      <input class="js-todo-input" type="text" aria-label="Enter a new todo item" placeholder="E.g. Build a web app" bind:value={newTodo}>
+      <input
+        class="js-todo-input"
+        type="text"
+        aria-label="Enter a new todo item"
+        placeholder="E.g. Build a web app"
+        bind:value={newTodo} />
     </form>
   </div>
 </main>
