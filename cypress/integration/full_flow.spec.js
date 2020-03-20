669f80981f116a64062f97d89ec06660db7c44b6
Paul Hebert <paul@cloudfour.com>
2020-03-20 10:55:16 -0700

    Final test refactors
    - Update comments for clarity
    - Use shorthand when typing `{enter}` in inputs
    - Fetch "done" checkbox by item text
    - Add note about brittle text selectors
    
    Co-authored-by: Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>

--- cypress/integration/full_flow.spec.js
+++ cypress/integration/full_flow.spec.js
@@ -1,22 +1,18 @@
-/**
- * Refactoring considerations
- * @see https://docs.cypress.io/guides/references/best-practices.html#Selecting-Elements
- * Can we DRY the get() at all?
- */
 it("Should add, complete, and remove items", function() {
   cy.visit("/");
 
+  // I know we were trying to write the test from a user's perspective,
+  // but what if the placeholder value changes? This feels a bit brittle. 
   const PLACEHOLDER_VAL = "[placeholder='E.g. Build a web app']";
+  // What happens if the heading changes?
+  // Should we use a `data-test` attribute? 
   const EMPTY_STATE_TEXT = "Add your first todo";
 
-  // cy.log("Check our empty state...");
+  // cy.log("Check that we can see our empty state");
   cy.contains(EMPTY_STATE_TEXT).should("be.visible");
 
   // cy.log('Enter "test 1" into form input');
-  cy.get(PLACEHOLDER_VAL).type("test 1");
-
-  // cy.log('Press "enter" to submit our form');
-  cy.get(PLACEHOLDER_VAL).type("{enter}");
+  cy.get(PLACEHOLDER_VAL).type("test 1{enter}");
 
   // cy.log("Check that there is a ToDo list item with the text 'test 1'");
   cy.get("[data-cy='todo-item']").should("contain", "test 1");
@@ -26,31 +22,27 @@ it("Should add, complete, and remove items", function() {
 
   // cy.log("Add 2 more items");
   cy.get(PLACEHOLDER_VAL)
-    .type("test 2")
-    .type("{enter}");
-  cy.get(PLACEHOLDER_VAL)
-    .type("test 3")
-    .type("{enter}");
+    .type("test 2{enter}")
+    .type("test 3{enter}");
 
   // cy.log("Check that there are now 3 items");
   cy.get("[data-cy='todo-item']").should("have.length", 3);
 
   // cy.log("Mark the first item as done");
-  cy.get("[data-cy='visual-checkbox']")
-    .first()
+  cy.contains("[data-cy='todo-item']", "test 1")
+    .find("[data-cy='visual-checkbox']")
     .click();
 
   // cy.log("Confirm the first item has the done class");
-  cy.get("[data-cy='todo-item']")
-    .first()
+  cy.contains("[data-cy='todo-item']", "test 1")
     .should("have.class", "done");
 
   // cy.log("Confirm the first item's checkbox is checked'");
-  cy.get("[data-cy='hidden-checkbox']")
-    .first()
+  cy.contains("[data-cy='todo-item']", "test 1")
+    .find("[data-cy='hidden-checkbox']")
     .should("be.checked");
 
-  // cy.log("Delete our first item by clicking the 'X' button");
+  // cy.log("Delete our first item by clicking the 'x' button next to 'test 2'");
   cy.contains("[data-cy='todo-item']", "test 2")
     .find("[data-cy='delete-button']")
     .click();
@@ -58,7 +50,7 @@ it("Should add, complete, and remove items", function() {
   // cy.log("Check that there are now 2 items");
   cy.get("[data-cy='todo-item']").should("have.length", 2);
 
-  // cy.log("Delete our remaining elements");
+  // cy.log("Delete another ToDo item");
   cy.contains("[data-cy='todo-item']", "test 1")
     .find("[data-cy='delete-button']")
     .click();
@@ -66,6 +58,7 @@ it("Should add, complete, and remove items", function() {
   // cy.log("Check that there is now 1 item");
   cy.get("[data-cy='todo-item']").should("have.length", 1);
 
+  // cy.log("Delete our final ToDo item");
   cy.contains("[data-cy='todo-item']", "test 3")
     .find("[data-cy='delete-button']")
     .click();
@@ -73,6 +66,6 @@ it("Should add, complete, and remove items", function() {
   // cy.log("Check that there are now 0 items");
   cy.get("[data-cy='todo-item']").should("not.exist");
 
-  // cy.log("Check our empty state...");
+  // cy.log("Check that we can see our empty state");
   cy.contains(EMPTY_STATE_TEXT).should("be.visible");
 });
